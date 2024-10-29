fce60dbbaadead872d12dd98e304950f32616b8d
DenKhaziev <110347267+DenKhaziev@users.noreply.github.com>
2024-10-29 22:15:55 +0300

    Add files via upload

---
+++ process.php
@@ -0,0 +1,53 @@
+<?php
+$products = [
+        [
+            'title' => 'Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport',
+            'price' => 599,
+            'rating' => 5.0,
+            'description' => 'Самые продвинутые часы Apple.',
+            'image' => 'https://happyhaha.github.io/css/dist/img/apple-watch.webp',
+            'tags' => ['часы', 'Телефоны']
+        ],
+        [
+            'title' => 'Apple iPhone 14 Pro, 128GB, фиолетовый',
+            'price' => 999,
+            'rating' => 4.9,
+            'description' => 'Мощный смартфон с потрясающей камерой.',
+            'image' => 'https://happyhaha.github.io/css/dist/img/iphone14.webp',
+            'tags' => ['Ноутбуки', 'Телефоны']
+        ],
+        [
+            'title' => 'Samsung Galaxy Watch 5, черныйб 44мм',
+            'price' => 299,
+            'rating' => 3.0,
+            'description' => 'Стильные и функциональные умные часы.',
+            'image' => 'https://happyhaha.github.io/css/dist/img/galaxy.webp',
+            'tags' => ['часы', 'Телефоны']
+        ],
+        [
+            'title' => 'Dell XPS 13 Laptop',
+            'price' => 1200,
+            'rating' => 2.5,
+            'description' => 'Стильные и функциональные умные часы.',
+            'image' => 'https://happyhaha.github.io/css/dist/img/dell.webp',
+            'tags' => ['Ноутбуки', 'Телефоны']
+        ],
+        [
+            'title' => 'Apple MacBook Air M2',
+            'price' => 1299,
+            'rating' => 5.0,
+            'description' => 'Стильные и функциональные умные часы.',
+            'image' => 'https://happyhaha.github.io/css/dist/img/macbook.webp',
+            'tags' => ['Ноутбуки', 'Телефоны']
+        ],
+        [
+            'title' => 'Xiaomi Mi Band 7',
+            'price' => 59,
+            'rating' => 1.0,
+            'description' => 'Стильные и функциональные умные часы.',
+            'image' => 'https://happyhaha.github.io/css/dist/img/watch.webp',
+            'tags' => ['часы', 'Телефоны']
+        ],
+];
+
+?>
